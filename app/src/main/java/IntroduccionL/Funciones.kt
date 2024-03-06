6eb71b0e39e83e280b46576ab672179f526544fa
liz-yuliana <liz.velandia@urosario.edu.co>
2024-03-05 19:00:48 -0500

    Clase y Quiz
    Se sube la clase sobre funciones y luego el quiz de realizar una calculadora con funciones

--- app/src/main/java/IntroduccionL/Funciones.kt
+++ app/src/main/java/IntroduccionL/Funciones.kt
@@ -3,3 +3,23 @@ package IntroduccionL
 class Funciones {
 }
 
+fun Hello(){
+    println("Hello, Liz! :D")
+}
+
+fun printHello(name:String?): Unit{
+
+    println("Hi There")
+}
+
+fun printHello1(name:String?){
+
+    println("Hi There " + name)
+}
+
+fun main(){
+
+    Hello()
+    printHello("Liz")
+    printHello1("Yuly")
+}
