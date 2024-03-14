e83c06c5fac99803964aa267d58ac616ab7376d0
liz-yuliana <liz.velandia@urosario.edu.co>
2024-03-14 17:41:58 -0500

    Taller_version2
    adelanto

--- app/src/main/java/IntroduccionL/Subasta/Oferta.kt
+++ app/src/main/java/IntroduccionL/Subasta/Oferta.kt
@@ -1,4 +1,15 @@
 package IntroduccionL.Subasta
 
 class Oferta {
+
+    /*Atributos*/
+
+    var ofertante:String
+    var oferta: Double
+
+    constructor(ofertante:String, oferta:Double){
+        this.ofertante = ofertante
+        this.oferta = oferta
+    }
+
 }
