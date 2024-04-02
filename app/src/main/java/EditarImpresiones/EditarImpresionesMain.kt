51defab01320a53a367d22f2947b89fbafdb98cd
jonecto <jonectado@gmail.com>
2024-04-02 07:35:49 -0500

    Arreglo de bug visual

---
+++ app/src/main/java/EditarImpresiones/EditarImpresionesMain.kt
@@ -0,0 +1,12 @@
+package EditarImpresiones
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.a3dlab.R
+
+class EditarImpresionesMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_editar_impresiones_main)
+    }
+}
