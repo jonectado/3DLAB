f66cd9741fd2b2bd16a234c951450381f5ec9b0f
jonecto <jonectado@gmail.com>
2024-05-13 17:04:09 -0500

    Arreglos menores

--- app/src/main/java/Notificaciones/Adaptador/ViewHolder_Notificaciones.kt
+++ app/src/main/java/Notificaciones/Adaptador/ViewHolder_Notificaciones.kt
@@ -12,27 +12,30 @@ import com.example.a3dlab.R
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
-class ViewHolder_Notificaciones (view:View):RecyclerView.ViewHolder (view){
+class ViewHolder_Notificaciones(view: View) : RecyclerView.ViewHolder(view) {
 
     val notificacion_mostrada = view.findViewById<TextView>(R.id.texto_noti_general)
     var img_mostrada = view.findViewById<ImageView>(R.id.img_general)
 
-    fun render(notificacionModel:Notificacion){
-        when (notificacionModel.notificacion_text){
+    fun render(notificacionModel: Notificacion) {
+        when (notificacionModel.notificacion_text) {
 
             "cama" -> {
                 notificacion_mostrada.text = "Deberias lavar la cama de impresión"
                 img_mostrada.setImageResource(R.drawable.impresion)
             }
+
             "ejes" -> {
                 notificacion_mostrada.text = "Deberias lubricar los ejes"
                 img_mostrada.setImageResource(R.drawable.impresion)
             }
+
             "filamento" -> {
 
                 Firebase.firestore.collection("Filamentos")
-                    .document(notificacionModel.filamento).get().addOnSuccessListener {
-                        fil -> notificacion_mostrada.text = "Se acaba el filamento ${fil.data!!["color"]} ${fil.data!!["marca"]}"
+                    .document(notificacionModel.filamento).get().addOnSuccessListener { fil ->
+                        notificacion_mostrada.text =
+                            "Se acaba el filamento ${fil.data!!["color"]} ${fil.data!!["marca"]}"
                     }
                 img_mostrada.setImageResource(R.drawable.filamento)
             }
