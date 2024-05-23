34ce61dda9e765b7e1c8b818b93e6c59da1f76fe
jonecto <jonectado@gmail.com>
2024-05-22 22:32:37 -0500

    Lista de usuarios: Done

--- app/src/main/java/Registro/Registro.kt
+++ app/src/main/java/Registro/Registro.kt
@@ -6,15 +6,19 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.example.a3dlab.R
+import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.firestore
+import com.google.firebase.firestore.snapshots
 
 class Registro : AppCompatActivity() {
     private lateinit var editTextTextEmailAddress: EditText
     private lateinit var editTextTextPassword: EditText
-    private lateinit var editTextTextPassword2:EditText
+    private lateinit var editTextTextPassword2: EditText
     private lateinit var button2: Button
     private lateinit var button3: Button
     private lateinit var firebaseAuth: FirebaseAuth
+    private var id = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
@@ -24,6 +28,8 @@ class Registro : AppCompatActivity() {
         editTextTextPassword2 = findViewById(R.id.editTextTextPassword2)
         button2 = findViewById(R.id.button2)
         button3 = findViewById(R.id.button3)
+
+        setId()
 
         button2.setOnClickListener {
             registrar()
@@ -37,17 +43,34 @@ class Registro : AppCompatActivity() {
         val email = editTextTextEmailAddress.text.toString()
         val pass = editTextTextPassword.text.toString()
         val confirmPass = editTextTextPassword2.text.toString()
+        Toast.makeText(
+            this,
+            "Cargando...",
+            Toast.LENGTH_SHORT
+        ).show()
         if (email.isNotEmpty() && pass.isNotEmpty()) {
-            if(pass == confirmPass){
-                firebaseAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener{
-                    if(it.isSuccessful){
-                        Toast.makeText(
-                            this,
-                            "Usuario añadido correctamente",
-                            Toast.LENGTH_SHORT
-                        ).show()
+            if (pass == confirmPass) {
+                firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        Firebase.firestore.collection("Usuarios")
+                            .document(id)
+                            .set(
+                                mapOf<String, String>(
+                                    Pair(
+                                        "correo",
+                                        firebaseAuth.currentUser!!.email.toString()
+                                    )
+                                )
+                            )
+                            .addOnSuccessListener {
+                                Toast.makeText(
+                                    this,
+                                    "Usuario añadido correctamente",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
                         finish()
-                    }else{
+                    } else {
                         Toast.makeText(
                             this,
                             it.exception.toString(),
@@ -55,7 +78,7 @@ class Registro : AppCompatActivity() {
                         ).show()
                     }
                 }
-            }else{
+            } else {
                 Toast.makeText(
                     this,
                     "Las contraseñas no coinciden",
@@ -70,5 +93,20 @@ class Registro : AppCompatActivity() {
                 Toast.LENGTH_SHORT
             ).show()
         }
+    }
+
+    fun setId() {
+        var userId = listOf<Int>()
+        Firebase.firestore.collection("Usuarios").get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    userId = userId.plus(document.id.toInt())
+                }
+                id = if (userId.isEmpty()) {
+                    "1"
+                } else {
+                    (userId.max() + 1).toString()
+                }
+            }
     }
 }
--- app/src/main/java/usuario/usuario.kt
+++ app/src/main/java/usuario/usuario.kt
@@ -1,6 +1,5 @@
 package usuario
 
-import Login.LogInMain
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
@@ -9,7 +8,15 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.a3dlab.R
 import com.google.firebase.auth.FirebaseAuth
 import Login.passwordreset
+import android.util.Log
+import android.widget.ArrayAdapter
 import android.widget.ImageButton
+import android.widget.ListView
+import android.widget.Toast
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import com.google.firebase.firestore.firestore
 
 
 class usuario : AppCompatActivity() {
@@ -17,18 +24,29 @@ class usuario : AppCompatActivity() {
     private var userEmail:String = ""
     private lateinit var backbutton: ImageButton
     private lateinit var button2: Button
+    private lateinit var lista: ListView
+    private lateinit var swipe: SwipeRefreshLayout
+    private var lista_usuarios:ArrayList<String> = arrayListOf<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_usuario)
         backbutton = findViewById(R.id.backButton)
         button2 = findViewById(R.id.button2)
-
+        lista = findViewById(R.id.listView)
+        updateUsers()
         val user = FirebaseAuth.getInstance().currentUser
         if (user != null) {
             userEmail = user.email.toString()
         } else {
             // No user is signed in
         }
+        swipe = findViewById(R.id.swipeswipe)
+        swipe.setColorSchemeResources(R.color.buttons)
+        swipe.setOnRefreshListener {
+            updateUsers()
+            swipe.isRefreshing = false
+        }
+
         email = findViewById(R.id.textView4)
         email.text = userEmail
 
@@ -40,5 +58,22 @@ class usuario : AppCompatActivity() {
             val intent = Intent(this, passwordreset::class.java)
             startActivity(intent)
         }
+
+        var itemsAdapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, lista_usuarios)
+    }
+
+    fun updateUsers(){
+        Firebase.firestore.collection("Usuarios")
+            .get()
+            .addOnSuccessListener { db ->
+                for(document in db){
+                    Log.d("Aqui", "updateUsers: ${document.data["correo"]}")
+                    lista_usuarios.add(document.data["correo"].toString())
+                }
+                val itemsAdapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, lista_usuarios)
+                lista.adapter = itemsAdapter
+                Toast.makeText(this, "finalizó", Toast.LENGTH_LONG).show()
+            }
+
     }
 }
